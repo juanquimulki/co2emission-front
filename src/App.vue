676d9cffd8396e451d366d51a826333b41cc82ba
Juanqui Mulki <juanquimulki@hotmail.com>
2021-11-29 13:00:55 -0300

    Axios request

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,26 @@
       <b-navbar-brand>CO2 Emissions</b-navbar-brand>
     </b-navbar>
     <div class="content">
+      <b-card>
+        <b-row>
+          <b-col md="1">
+            <b-form-group label="From:">
+              <b-form-select v-model="from" :options="years"></b-form-select>
+            </b-form-group>
+          </b-col>
+          <b-col md="1">
+            <b-form-group label="To:">
+              <b-form-select v-model="to" :options="years"></b-form-select>
+            </b-form-group>
+          </b-col>
+          <b-col md="1">
+            <b-form-group label="Options:">
+              <b-button @click="submit">Submit</b-button>
+            </b-form-group>
+          </b-col>
+        </b-row>
+      </b-card>
+
       <b-overlay :show="showOverlay" rounded="sm">
         <b-card>
           <b-table hover :items="items" :fields="fields"></b-table>
@@ -14,11 +34,15 @@
 </template>
 
 <script>
+import _axios from "../src/common/apiClient";
+
 export default {
   name: "App",
   components: {},
   data() {
     return {
+      api_url: "http://localhost/co2emission-api/public/",
+
       items: [],
       fields: [
         { key: "state", label: "State", sortable: true },
@@ -27,24 +51,47 @@ export default {
         { key: "value", sortable: true },
       ],
       showOverlay: false,
+      states: [],
+
+      from: 1980, //process.env.VUE_APP_YEAR_MIN,
+      to: 2018, //process.env.VUE_APP_YEAR_MAX,
+      years: [],
     };
   },
   async created() {
-    this.showOverlay = true;
-    let i = 0;
-    for (i = 1; i <= 10; i++) {
-      await this.loadData();
-    }
-    this.showOverlay = false;
+    this.setUpYears();
   },
   methods: {
-    async loadData() {
-      await this.axios
+    setUpYears() {
+      for (var i = this.from; i <= this.to; i++) {
+        this.years.push(i);
+      }
+    },
+    async submit() {
+      this.showOverlay = true;
+      this.items = [];
+      await _axios
+        .get(this.api_url + "/state")
+        .then((response) => {
+          this.states = response.data.result;
+        });
+
+      for (var i = 1; i < this.states.length; i++) {
+        await this.loadData(this.states[i]);
+      }
+      this.showOverlay = false;
+    },
+    async loadData(state) {
+      await _axios
         .get(
-          "http://localhost/co2emission-api/public/emission/California/2003/2006"
+          this.api_url + "emission/" +
+            state +
+            "/" +
+            this.from +
+            "/" +
+            this.to
         )
         .then((response) => {
-          console.log(response.data);
           this.items.push(response.data.result[0]);
         });
     },
@@ -73,5 +120,9 @@ body {
 
 .sr-only {
   display: none !important;
+}
+
+.card {
+  margin-bottom: 10px;
 }
 </style>
---
+++ src/common/apiClient.js
@@ -0,0 +1,7 @@
+import axios from "axios";
+
+const _axios = axios.create({
+  baseURL: process.env.VUE_APP_API_ROUTE,
+});
+
+export default _axios;
